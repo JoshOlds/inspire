3e23beffb1f517ac9e0c701de7b2fa8e9f0f1611
Joshua Olds <joshua.olds@outlook.com>
2016-10-17 16:11:10 -0600

    Icon HTTPS

--- app/components/weather/weather-service.js
+++ app/components/weather/weather-service.js
@@ -2,7 +2,7 @@ function WeatherService() {
 	var url = '//bcw-getter.herokuapp.com/?url=';
 	var url2 = 'http://api.openweathermap.org/data/2.5/weather?q=boise&&APPID=bd82255fd0a21fa1238699b9eda2ee35';
 	var apiUrl = url + encodeURIComponent(url2);
-	var apiWeatherIconUrl = 'http://openweathermap.org/img/w/'
+	var apiWeatherIconUrl = 'https://openweathermap.org/img/w/'
 
 	this.getWeather = function (callWhenDone) {
 
