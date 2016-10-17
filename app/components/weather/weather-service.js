497034ce131be8430a9832f038b910aff239dd30
Joshua Olds <joshua.olds@outlook.com>
2016-10-17 17:14:10 -0600

    Chenged weather to use geolocation

--- app/components/weather/weather-service.js
+++ app/components/weather/weather-service.js
@@ -1,15 +1,20 @@
 function WeatherService() {
 	var url = '//bcw-getter.herokuapp.com/?url=';
-	var url2 = 'http://api.openweathermap.org/data/2.5/weather?q=boise&&APPID=bd82255fd0a21fa1238699b9eda2ee35';
+	var url2 = 'http://api.openweathermap.org/data/2.5/weather?lat=replacelat&lon=replacelon&&APPID=bd82255fd0a21fa1238699b9eda2ee35';
 	var apiUrl = url + encodeURIComponent(url2);
 	var apiWeatherIconUrl = 'https://openweathermap.org/img/w/'
 
 	this.getWeather = function (callWhenDone) {
 
-		$.get(apiUrl, function (res) {
-			var obj = JSON.parse(res)
-			callWhenDone(obj);
+		navigator.geolocation.getCurrentPosition(function(geo){
+			var locationURL = apiUrl.replace("replacelon", geo.coords.longitude);
+			locationURL = locationURL.replace("replacelat", geo.coords.latitude);
+			$.get(locationURL, function (res) {
+				var obj = JSON.parse(res)
+				callWhenDone(obj);
+			})
 		})
+		
 	}
 
 	this.getWeatherIcon = function (iconID){
