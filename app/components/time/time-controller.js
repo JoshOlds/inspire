33ff2167f48c3bc0a72e9a838e1df52604830b75
Joshua Olds <joshua.olds@outlook.com>
2016-10-16 21:52:10 -0600

    BedTime

---
+++ app/components/time/time-controller.js
@@ -0,0 +1,32 @@
+(function () {
+
+    displayTime(true);
+
+    function displayTime(boolFormat) {
+        var date = new Date();
+        var halfFormat = formatAMPM(date);
+        var monthDisplay = date.getDate() + ' ' + getMonthName(date) ;
+
+        $('#clock').html(halfFormat);
+        $('#date').html(monthDisplay)
+    }
+
+    function getMonthName(date) {
+        var monthNames = ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        ];
+        return monthNames[date.getMonth()];
+    }
+
+    function formatAMPM(date) {
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        var strTime = hours + ':' + minutes;
+        return strTime;
+    }
+
+} ())
